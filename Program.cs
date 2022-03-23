832d1d0c9a7a9bba9d7c175113da5d76d54b1d96
Dmitry <dima.fenix@list.ru>
2022-03-23 18:30:03 +0300

    Empty progect for piramid solitaire

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiramidSolitaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
